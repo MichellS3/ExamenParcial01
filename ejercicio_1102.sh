d139d35977470abb667b43dcd87bb337a2786842
MichellS3 <salcanshirley1b@gmail.com>
2022-01-12 13:26:55 -0500

    Literal A, bien hecho :v

--- ejercicio_1102.sh
+++ ejercicio_1102.sh
@@ -3,8 +3,11 @@
 cd ../
 cd CSB-master/unix/data/
 
-cut -d "	" -f 1 Gesquiere2011_data.csv | grep -w "3" | grep -c "3" $w
-cut -d "	" -f 1 Gesquiere2011_data.csv | grep -w "27" | grep -c "27" $y
+#Literal A
+echo "¿Cuántas veces se registraron los niveles de los individuos 3 y 27?"
 
-echo "Número de veces que se registraron los niveles del individuo 3: " $w
-echo "Número de veces que se registraron los niveles del individuo 27: " $y
+a=`cut -f 1 Gesquiere2011_data.csv | grep -w "3" | grep -c "3"`
+b=`cut -f 1 Gesquiere2011_data.csv | grep -w "27" | grep -c "27"`
+
+echo Número de veces que se registraron los niveles del individuo 3 es=  $a
+echo Número de veces que se registraron los niveles del individuo 27 es=  $b
