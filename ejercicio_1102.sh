3b9ea5bd929be0ea02eb0f79c8a2cefb5088fd11
MichellS3 <salcanshirley1b@gmail.com>
2022-01-12 12:29:32 -0500

    Ejercicio 1.11.02

--- ejercicio_1102.sh
+++ ejercicio_1102.sh
@@ -6,5 +6,5 @@ cd CSB-master/unix/data/
 cut -d "	" -f 1 Gesquiere2011_data.csv | grep -w "3" | grep -c "3" $w
 cut -d "	" -f 1 Gesquiere2011_data.csv | grep -w "27" | grep -c "27" $y
 
-echo "Número de veces que se registraron los niveles del individuo 3: $w " $w
+echo "Número de veces que se registraron los niveles del individuo 3: " $w
 echo "Número de veces que se registraron los niveles del individuo 27: " $y
